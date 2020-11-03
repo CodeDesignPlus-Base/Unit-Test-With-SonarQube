2968c93da2ebd0ade6ab1432c990ea30c6ad9c27
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-03 18:38:05 -0500

    Se agrega el archivo de versionamiento

--- nuget.ps1
+++
@@ -1,5 +0,0 @@
-dotnet pack -o .\
-
-dotnet nuget push Calculator.1.0.0.nupkg --api-key oy2cv26wojkxvdm3ffgqd6inurn4j7bexjgbwxh4x2l52i --source https://api.nuget.org/v3/index.json
-
-# dotnet nuget push
