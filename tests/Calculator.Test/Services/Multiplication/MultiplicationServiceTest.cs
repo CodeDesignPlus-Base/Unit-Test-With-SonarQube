458b3d9ec67982c857566cdbfa54ba16cf039259
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-04 10:39:37 -0500

    Se actualizan las pruebas unitarias

--- tests/Calculator.Test/Services/Multiplication/MultiplicationServiceTest.cs
+++ tests/Calculator.Test/Services/Multiplication/MultiplicationServiceTest.cs
@@ -5,33 +5,33 @@ namespace Calculator.Test.Services.Multiplication
 {
     public class MultiplicationServiceTest
     {
-        //[Fact]
-        //public void Calculate_Multiplication_TwentyFive()
-        //{
-        //    var a = 5;
-        //    var b = 5;
-        //    var expected = a * b;
+        [Fact]
+        public void Calculate_Multiplication_TwentyFive()
+        {
+            var a = 5;
+            var b = 5;
+            var expected = a * b;
 
-        //    var service = new MultiplicationService();
+            var service = new MultiplicationService();
 
-        //    var result = service.Calculate(a, b);
+            var result = service.Calculate(a, b);
 
-        //    Assert.Equal(expected, result);
-        //}
+            Assert.Equal(expected, result);
+        }
 
 
-        //[Theory]
-        //[InlineData(5, 15, 75)]
-        //[InlineData(25, 1, 25)]
-        //[InlineData(40, 45, 1800)]
-        //[InlineData(5, 10, 50)]
-        //public void Calculate_Multiplication_ResultEqualsExpected(int a, int b, int expected)
-        //{
-        //    var service = new MultiplicationService();
+        [Theory]
+        [InlineData(5, 15, 75)]
+        [InlineData(25, 1, 25)]
+        [InlineData(40, 45, 1800)]
+        [InlineData(5, 10, 50)]
+        public void Calculate_Multiplication_ResultEqualsExpected(int a, int b, int expected)
+        {
+            var service = new MultiplicationService();
 
-        //    var result = service.Calculate(a, b);
+            var result = service.Calculate(a, b);
 
-        //    Assert.Equal(expected, result);
-        //}
+            Assert.Equal(expected, result);
+        }
     }
 }
