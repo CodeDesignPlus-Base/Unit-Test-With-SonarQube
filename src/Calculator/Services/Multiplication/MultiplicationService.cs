5d12863597bc78066cce74e5b219d875155c7d36
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-04 08:41:46 -0500

    Validando el pipeline

--- src/Calculator/Services/Multiplication/MultiplicationService.cs
+++ src/Calculator/Services/Multiplication/MultiplicationService.cs
@@ -4,11 +4,6 @@
     {
         public int Calculate(int a, int b)
         {
-            return this.Process(a,b);
-        }
-
-        private int Process(int a, int b)
-        {
             return a * b;
         }
     }
