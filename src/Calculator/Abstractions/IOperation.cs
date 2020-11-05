dea8ba359cfb90bd528d32109be0c0979142ff44
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-05 18:23:12 -0500

    Se agrega la documentación

--- src/Calculator/Abstractions/IOperation.cs
+++ src/Calculator/Abstractions/IOperation.cs
@@ -5,6 +5,12 @@
     /// </summary>
     public interface IOperation
     {
+        /// <summary>
+        /// Metodo encargado de calcular segun la implementación de la operación
+        /// </summary>
+        /// <param name="a">Valor 1</param>
+        /// <param name="b">Valor 2</param>
+        /// <returns>Retorna el resultado del calculo</returns>
         int Calculate(int a, int b);
     }
 }
