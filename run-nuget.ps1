992792dc4358b1cab7bd60cc399ab0d55813d654
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-05 18:19:04 -0500

    Se actualizan las propiedades del paquete

---
+++ run-nuget.ps1
@@ -0,0 +1,3 @@
+dotnet pack src/Calculator/Calculator.csproj
+
+dotnet nuget push CodeDesignPlusCalculator.1.0.0.nupkg --api-key qz2jga8pl3dvn2akksyquwcs9ygggg4exypy3bhxy6w6x6 --source https://api.nuget.org/v3/index.json
