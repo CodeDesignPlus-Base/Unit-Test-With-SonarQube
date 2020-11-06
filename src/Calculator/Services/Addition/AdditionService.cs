b27545c84a5b5f3988d92cef22b288f5b5c4665b
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-11-05 19:05:03 -0500

    Se actualizan los comentarios

--- src/Calculator/Services/Addition/AdditionService.cs
+++ src/Calculator/Services/Addition/AdditionService.cs
@@ -2,6 +2,12 @@
 {
     public class AdditionService : IAdditionService
     {
+        /// <summary>
+        /// Metodo encargado de calcular segun la implementación de la operación
+        /// </summary>
+        /// <param name="a">Valor 1</param>
+        /// <param name="b">Valor 2</param>
+        /// <returns>Retorna el resultado del calculo</returns>
         public int Calculate(int a, int b)
         {
             return a + b;
